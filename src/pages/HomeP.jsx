3aca3ffaf202f2740eb0f0efff5dac6a9b3c37fd
abdumalikweb <abdumalikweb@gmail.com>
2023-04-10 08:15:40 +0500

    ready

---
+++ src/pages/HomeP.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminLayout from '../components/AdminLayout'
+
+const HomeP = () => {
+  return (
+    <AdminLayout >
+      <div>homeP</div>
+    </AdminLayout>
+    
+  )
+}
+
+export default HomeP
+
